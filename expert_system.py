bf81ebabee9fbefa62d7ed5c61f3e097a0779dd0
Madjid-CH <a.chergui@esi-sba.dz>
2022-11-16 14:11:21 +0100

    modifying forward chaining and adding acceptance tests

--- expert_system.py
+++ expert_system.py
@@ -1,27 +1,41 @@
 def forward_chaining(fact, facts, rules):
     rules_stack = rules.copy()
+    rules_stack_2 = []
     rules_applied = []
+
     if fact in facts:
         return True, rules_applied, facts
     else:
         for rule in rules_stack:
-            if premise_in_facts(rule, facts):
-                facts = union_consequence(facts, rule)
-                save_rule_index(rule, rules, rules_applied)
-                rules_stack.pop(rules_stack.index(rule))
-
-        stop = False
-        while not stop:
-            stop = True
-            for rule in rules_stack[::-1]:
-                if premise_in_facts(rule, facts):
+            if is_premise_in_facts(rule, facts):
+                if not is_consequence_in_facts(rule, facts):
                     facts = union_consequence(facts, rule)
                     save_rule_index(rule, rules, rules_applied)
-                    rules_stack.pop(rules_stack.index(rule))
-                    stop = False
+                    # rules_stack.pop(rules_stack.index(rule))
+            else:
+                rules_stack_2.append(rule)
 
+        stop = False
+        while not stop and (fact not in facts):
+            stop = True
+            for rule in rules_stack_2[::-1]:
+                if is_premise_in_facts(rule, facts):
+                    if not is_consequence_in_facts(rule, facts):
+                        facts = union_consequence(facts, rule)
+                        save_rule_index(rule, rules, rules_applied)
+                        rules_stack_2.pop(rules_stack_2.index(rule))
+                        stop = False
 
+    print(rules_stack_2)
     return fact in facts, rules_applied, facts
+
+
+def is_premise_in_facts(rule, facts):
+    return list(rule.keys())[0].issubset(facts)
+
+
+def is_consequence_in_facts(rule, facts):
+    return rule.get(list(rule.keys())[0]).issubset(facts)
 
 
 def save_rule_index(rule, rules, rules_applied):
@@ -30,7 +44,3 @@ def save_rule_index(rule, rules, rules_applied):
 
 def union_consequence(facts, rule):
     return facts.union(rule.get(list(rule.keys())[0]))
-
-
-def premise_in_facts(rule, facts):
-    return list(rule.keys())[0].issubset(facts)
--- test_expert_system.py
+++ test_expert_system.py
@@ -6,59 +6,78 @@ def test_empty_facts_base():
 
 
 def test_fact_in_facts_base():
-    assert forward_chaining("A", frozenset({"A"}), []) == (True, [], {"A"})
-    assert forward_chaining("B", frozenset(["A"]), []) == (False, [], {"A"})
+    assert forward_chaining("A", {"A"}, []) == (True, [], {"A"})
+    assert forward_chaining("B", {"A"}, []) == (False, [], {"A"})
 
 
 def test_one_step():
-    facts = frozenset(["A", "B"])
-    assert forward_chaining("B", frozenset({"A"}), [{frozenset(["A"]): frozenset({"B"})}]) == \
+    facts = {"A", "B"}
+    assert forward_chaining("B", frozenset({"A"}), [{frozenset(["A"]): {"B"}}]) == \
            (True, [0], {"A", "B"})
-    assert forward_chaining("C", facts, [{frozenset({"A", "B"}): frozenset({"C"})}]) == \
+    assert forward_chaining("C", facts, [{frozenset({"A", "B"}): {"C"}}]) == \
            (True, [0], {"A", "B", "C"})
-    assert forward_chaining("D", facts, [{frozenset({"A", "B"}): frozenset({"C"})}]) == \
+    assert forward_chaining("D", facts, [{frozenset({"A", "B"}): {"C"}}]) == \
            (False, [0], {"A", "B", "C"})
 
 
 def test_two_forward_steps():
-    facts = frozenset(["A", "B"])
-    rules = [{frozenset({"A", "B"}): frozenset({"D"})},
-             {frozenset({"D"}): frozenset({"C"})}]
+    facts = {"A", "B"}
+    rules = [{frozenset({"A", "B"}): {"D"}},
+             {frozenset({"D"}): {"C"}}]
     assert forward_chaining("C", facts, rules) == (True, [0, 1], {'C', 'D', 'A', 'B'})
     assert forward_chaining("E", facts, rules) == (False, [0, 1], {'C', 'D', 'A', 'B'})
 
 
 def test_two_non_forward_steps():
-    facts = frozenset(["A", "B"])
-    rules = [{frozenset({"C"}): frozenset({"D"})},
-             {frozenset({"B"}): frozenset({"C"})}]
+    facts = {"A", "B"}
+    rules = [{frozenset({"C"}): {"D"}},
+             {frozenset({"B"}): {"C"}}]
     assert forward_chaining("D", facts, rules) == (True, [1, 0], {'D', 'B', 'A', 'C'})
     assert forward_chaining("J", facts, rules) == (False, [1, 0], {'D', 'B', 'A', 'C'})
 
 
 def test_three_non_forward_steps():
-    facts = frozenset(["A", "B"])
-    rules = [{frozenset({"C"}): frozenset({"D"})},
-             {frozenset({"B"}): frozenset({"C"})},
-             {frozenset({"D"}): frozenset({"E"})}]
+    facts = {"A", "B"}
+    rules = [{frozenset({"C"}): {"D"}},
+             {frozenset({"B"}): {"C"}},
+             {frozenset({"D"}): {"E"}}]
     assert forward_chaining("E", facts, rules) == (True, [1, 0, 2], {'B', 'D', 'C', 'A', 'E'})
     assert forward_chaining("J", facts, rules) == (False, [1, 0, 2], {'B', 'D', 'C', 'A', 'E'})
 
 
 def test_four_non_forward_steps():
-    facts = frozenset(["A", "B"])
-    rules = [{frozenset({"C"}): frozenset({"D"})},
-             {frozenset({"H"}): frozenset({"C"})},
-             {frozenset({"D"}): frozenset({"E"})},
-             {frozenset({"B"}): frozenset({"H"})}, ]
+    facts = {"A", "B"}
+    rules = [{frozenset({"C"}): {"D"}},
+             {frozenset({"H"}): {"C"}},
+             {frozenset({"D"}): {"E"}},
+             {frozenset({"B"}): {"H"}}, ]
     assert forward_chaining("E", facts, rules) == (True, [3, 1, 0, 2], {'B', 'D', 'H', 'E', 'C', 'A'})
     assert forward_chaining("P", facts, rules) == (False, [3, 1, 0, 2], {'B', 'D', 'H', 'E', 'C', 'A'})
 
 def test_loop_in_rules():
-    facts = frozenset(["A", "B"])
-    rules = [{frozenset({"C"}): frozenset({"D"})},
-             {frozenset({"D"}): frozenset({"C"})},]
+    facts = {"A", "B"}
+    rules = [{frozenset({"C"}): {"D"}},
+             {frozenset({"D"}): {"C"}},]
     assert forward_chaining("D", facts, rules) == (False, [], {'B', 'A'})
 
 def test_exo_1():
-    pass
+    facts = {"E", "F"}
+    rules = [{frozenset({"E", "B"}): {"C"}},
+             {frozenset({"F", "D"}): {"A"}},
+             {frozenset({"D", "E"}): {"B"}},
+             {frozenset({"B", "D"}): {"F"}},
+             {frozenset({"E", "F"}): {"D"}}, ]
+    assert forward_chaining("C", facts, rules) == (True, [4, 2, 1, 0], {'E', 'F', 'D', 'A', 'C', 'B'})
+
+def test_exo_2():
+    facts = {"B", "C"}
+    rules = [{frozenset({"D", "B", "E"}): {"F"}},
+             {frozenset({"G", "D"}): {"A"}},
+             {frozenset({"C", "F"}): {"A"}},
+             {frozenset({"B"}): {"X"}},
+             {frozenset({"D"}): {"E"}},
+             {frozenset({"X", "A"}): {"H"}},
+             {frozenset({"C"}): {"D"}},
+             {frozenset({"X", "C"}): {"A"}},
+             {frozenset({"X", "B"}): {"D"}}, ]
+    assert forward_chaining("H", facts, rules) == (True, [3, 6, 7, 5, 4, 0], {'E', 'C', 'B', 'F', 'D', 'X', 'A', 'H'})
