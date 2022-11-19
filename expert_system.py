6a824c60ca921972d986bc135f469960c2c2128e
Madjid-CH <a.chergui@esi-sba.dz>
2022-11-19 14:47:11 +0100

    finishing backward chaining

--- expert_system.py
+++ expert_system.py
@@ -52,34 +52,95 @@ def backward_chaining(fact, facts, rules):
     if fact in facts:
         return True, rules_applied, facts
     else:
-        rules_stack = get_backward_chain(fact, rules, facts)
-        for rule_index in rules_stack[::-1]:
-            if is_premise_in_facts(rules[rule_index], facts):
-                facts = apply_rule(facts, rules[rule_index], rules, rules_applied)
-                rules_applied.append(rule_index)
+        rules_stack = get_backward_chain(fact, rules, rules.copy(), facts, already_searched=set())
+        print(rules_stack)
+        remove_unnecessary_rules(rules_stack, rules)
+        stop = False
+        while not stop and (fact not in facts):
+            stop = True
+            for i in rules_stack:
+                if is_premise_in_facts(rules[i], facts):
+                    facts = apply_rule(facts, rules[i], rules, rules_applied)
+                    rules_stack.remove(i)
+                    stop = False
 
         return fact in facts, rules_applied, facts
 
 
-def get_backward_chain(fact, rules, already_searched=set()):
+def get_backward_chain(fact, rules, possible_rules, facts, already_searched=set()):
     rules_stack = []
-    if fact not in already_searched:
-        rule = rule_having_fact_in_consequence(fact, rules)
-        while rule and rules.index(rule) not in rules_stack:
+    if fact in already_searched:
+        return rules_stack
+
+    rules_of_the_fact = rules_having_fact_in_consequence(fact, possible_rules)
+    for rule in rules_of_the_fact:
+        while  rule in possible_rules and rules.index(rule) not in rules_stack:
             save_rule_index(rule, rules, rules_stack)
             already_searched.add(fact)
             for f in premise_of(rule):
-                chain = get_backward_chain(f, rules, already_searched)
+                chain = get_backward_chain(f, rules, possible_rules, facts, already_searched)
+                if len(chain) == 0 and f not in facts:
+                    break
                 [rules_stack.append(r) for r in chain if r not in rules_stack]
+
+    if len(rules_of_the_fact) == 0 and fact not in facts:
+        for r in rules_that_have_fact_in_premise(fact, rules):
+            for f in premise_of(rules[r]):
+                remove_the_rule_of(fact, possible_rules, rules_stack)
+                if f in already_searched:
+                    already_searched.remove(fact)
+
+
+
+    for rule in rules:
+        i = rules.index(rule)
+        if i in rules_stack and  rule not in possible_rules:
+            rules_stack.remove(i)
 
     return rules_stack
 
 
-def rule_having_fact_in_consequence(fact, rules):
+def remove_unnecessary_rules(rules_stack, rules):
+    count = dict()
+    indices_to_remove = []
+    for i in rules_stack:
+        fact = frozenset(consequence_of(rules[i]))
+        if count.get(fact):
+            count.update({fact: count.get(fact) + 1 })
+        else:
+            count.update({fact: 1 })
+        if count.get(fact) > 1:
+            indices_to_remove.append(i)
+
+    for i in indices_to_remove:
+        rules_stack.remove(i)
+
+def fact_not_provable(fact, facts, rule):
+    return not rule and fact not in facts
+
+
+def remove_the_rule_of(fact, rules, rules_indices):
+    indices = rules_that_have_fact_in_premise(fact, rules)
+    [rules_indices.remove(i) for i in indices if i in rules_indices]
+    values = [rules[i] for i in indices]
+    for v in values:
+        rules.remove(v)
+
+
+def rules_that_have_fact_in_premise(fact, rules):
+    result = []
+    for i in range(len(rules)):
+        if fact in premise_of(rules[i]):
+            result.append(i)
+    return result
+
+
+def rules_having_fact_in_consequence(fact, rules):
+    result = []
     for rule in rules:
         if fact in consequence_of(rule):
-            return rule
-    return None
+            result.append(rule)
+    return result
 
 
 def premise_of(rule):
--- tests/test_backword_chaining.py
+++ tests/test_backword_chaining.py
@@ -1,6 +1,6 @@
 import pytest
 
-from expert_system import backward_chaining, get_backward_chain
+from expert_system import backward_chaining, get_backward_chain, rules_that_have_fact_in_premise, remove_the_rule_of
 
 
 def test_empty_facts_base():
@@ -14,14 +14,38 @@ def test_fact_in_facts_base():
 
 def test_one_step():
     assert backward_chaining("B", {"A"}, [{"A": {"B"}}]) == (True, [0], {"A", "B"})
-    assert backward_chaining("C", {"A"}, [{("A"): {"B"}}]) == (False, [], {"A"})
+    assert backward_chaining("C", {"A"}, [{"A": {"B"}}]) == (False, [], {"A"})
 
 
 def test_two_step():
     facts = {"A", "B"}
     rules = [{("A", "B"): {"D"}},
-             {("D"): {"C"}}]
+             {"D": {"C"}}]
     assert backward_chaining("C", facts, rules) == (True, [0, 1], {'C', 'D', 'A', 'B'})
+
+
+def test_simple_chaining():
+    rules = [{"A": {"B"}}, ]
+    assert get_backward_chain("B", rules, rules.copy(), {'A'}, set()) == [0]
+    assert get_backward_chain("C", rules, rules.copy(), {'A'}, set()) == []
+
+
+def test_get_2_chaining():
+    rules = [{"A": {"D"}},
+             {"D": {"C"}}]
+    assert get_backward_chain("C", rules, rules.copy(), set('A'), set()) == [1, 0]
+    assert get_backward_chain("M", rules, rules.copy(), set('A'), set()) == []
+
+
+def test_get_3_chaining():
+    rules = [{"C": {"E"}},
+             {"A": {"D"}},
+             {"D": {"C"}},
+             {"K": {"M"}},
+             {"L": {"K"}},
+             ]
+    assert get_backward_chain("E", rules, rules.copy(), set('A'), set()) == [0, 2, 1]
+    assert get_backward_chain("M", rules, rules.copy(), {'A', 'L'}, set()) == [3, 4]
 
 
 def test_exo_1(setup_exo_1):
@@ -31,75 +55,64 @@ def test_exo_1(setup_exo_1):
 
 def test_exo_2(setup_exo_2):
     fact, facts, rules = setup_exo_2
-    assert backward_chaining(fact, facts, rules) == (    )
-
-
-def test_simple_chaining():
-    rules = [{("A"): {"B"}}, ]
-    assert get_backward_chain("B", rules, set()) == [0]
-    assert get_backward_chain("C", rules, set()) == []
-
-
-def test_get_2_chaining():
-    rules = [{("A"): {"D"}},
-             {("D"): {"C"}}]
-    assert get_backward_chain("C", rules, set()) == [1, 0]
-    assert get_backward_chain("M", rules, set()) == []
-
-
-def test_get_3_chaining():
-    rules = [{("C"): {"E"}},
-             {("A"): {"D"}},
-             {("D"): {"C"}},
-             {("K"): {"M"}},
-             {("L"): {"K"}},
-             ]
-    assert get_backward_chain("E", rules, set()) == [0, 2, 1]
-    assert get_backward_chain("M", rules, set()) == [3, 4]
+    assert backward_chaining(fact, facts.copy(), rules.copy()) == \
+           (True, [3, 6, 4, 0, 2, 5], {'A', 'X', 'E', 'D', 'C', 'B', 'F', 'H'})
 
 
 @pytest.mark.parametrize(
     'fact, rules, expected',
     (
-            ("E", [{("C"): {"E"}},
-                   {("E"): {"C"}}, ],
+            ("E", [{"C": {"E"}},
+                   {"E": {"C"}}, ],
              [0, 1]),
 
-            ("E", [{("C"): {"E"}},
-                   {("E"): {"F"}},
-                   {("F"): {"C"}}, ],
+            ("E", [{"C": {"E"}},
+                   {"E": {"F"}},
+                   {"F": {"C"}}, ],
              [0, 2, 1]),
     )
 )
 def test_loopy_chaining(fact, rules, expected):
-    assert get_backward_chain(fact, rules, set()) == expected
+    assert get_backward_chain(fact, rules, rules.copy(), set(), set()) == expected
 
 
 @pytest.mark.parametrize(
     'fact, rules, expected',
     (
-            ("F", [{("A"): {"E"}},
+            ("F", [{"A": {"E"}},
                    {("C", "E"): {"F"}},
-                   {("B"): {"C"}}],
+                   {"B": {"C"}}],
              [1, 2, 0]),
 
             ("E", [{("X", "Y", "Z"): {"E"}},
-                   {("A"): {"Z"}},
-                   {("B"): {"Y"}},
+                   {"A": {"Z"}},
+                   {"B": {"Y"}},
                    {("C", "B"): {"X"}},
                    ],
              [0, 3, 2, 1]),
 
             ("E", [{("A", "H"): {"Z"}},
                    {("X", "Y", "Z"): {"E"}},
-                   {("B"): {"Y"}},
+                   {"B": {"Y"}},
                    {("J", "K"): {"X"}},
-                   {("B"): {"K"}},
-                   {("C"): {"J"}},
-                   {("J"): {"H"}}
+                   {"B": {"K"}},
+                   {"C": {"J"}},
+                   {"J": {"H"}}
                    ],
              [1, 3, 5, 4, 2, 0, 6]),
     )
 )
 def test_multiple_facts_in_premise(fact, rules, expected):
-    assert get_backward_chain(fact, rules, set()) == expected
+    assert get_backward_chain(fact, rules, rules.copy(), {"A", "B", "C"}, set()) == expected
+
+
+def test_rules_remover(setup_exo_2):
+    fact, facts, rules = setup_exo_2
+    assert rules_that_have_fact_in_premise("X", rules) == [5, 7, 8]
+    remove_the_rule_of("X", rules, [])
+    assert rules == [{('D', 'B', 'E'): {'F'}},
+                     {('G', 'D'): {'A'}},
+                     {('C', 'F'): {'A'}},
+                     {'B': {'X'}},
+                     {'D': {'E'}},
+                     {'C': {'D'}}]
